9abd8a22a50fe3e3dc8c2986c766867b7b9985c2
ichigor <igor.moiano@hotmail.com>
2019-02-27 22:48:29 -0300

    Module 2 in progress

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,30 @@
+const { User } = require('../models')
+
+class SessionController {
+  async create (req, res) {
+    return res.render('auth/signin')
+  }
+
+  async store (req, res) {
+    const { email, password } = req.body
+    console.log('here')
+
+    const user = await User.findOne({ where: { email } })
+
+    if (!user) {
+      console.log('user not found')
+      return res.redirect('/')
+    }
+
+    if (!(await user.checkPassword(password))) {
+      console.log('password error')
+      return res.redirect('/')
+    }
+
+    req.session.user = user
+
+    return res.redirect('/app/dashboard')
+  }
+}
+
+module.exports = new SessionController()
